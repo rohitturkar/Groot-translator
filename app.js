b680b5076549c10ca52209bd259196f3ad4edc00
Rohit Turkar <rohittur2002@gmail.com>
2022-08-21 12:21:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var txtInput=document.querySelector("#txt-input");
-var btnTranslate=document.querySelector("#btn-translator");
-var outputDiv=document.querySelector("#output");
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translator");
+var outputDiv = document.querySelector("#output");
+var btnClear=document.querySelector("#btn-clear")
 
+var serverUrl = "https://api.funtranslations.com/translate/groot.json";
 
-btnTranslate.addEventListener("click",()=>{
-    outputDiv.innerText=txtInput.value;
+function getTranslationUrl(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error){
+    alert("there is something wrong in server!! , try after some time. "+error)
+}
+
+btnTranslate.addEventListener("click", () => {
+    var inputText = txtInput.value;
+    fetch(getTranslationUrl(inputText)).then(response => response.json()).then(json => outputDiv.innerHTML = json.contents.translated).catch(errorHandler)
 })
+
+btnClear.addEventListener("click",()=>{
+  location.reload();
+})
+
 
